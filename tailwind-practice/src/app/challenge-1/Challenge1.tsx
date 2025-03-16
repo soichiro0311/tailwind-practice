580529d250e289f3a1aceb045d0c72eb32658c28
清家蒼一朗 <soichiroseike@gmail.com>
2025-03-16 23:29:39 +0900

    apply responsive

--- tailwind-practice/src/app/challenge-1/Challenge1.tsx
+++ tailwind-practice/src/app/challenge-1/Challenge1.tsx
@@ -1,24 +1,31 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import MainVisualSvg from "../../svg/mainVisual.svg?react";
+import MainVisualSvg2 from "../../svg/mainVisual2.svg?react";
+import { useMediaQuery } from "react-responsive";
+
 import AboutSvg from "../../svg/about.svg?react";
 import Bicycle1Svg from "../../svg/bicycle1.svg?react";
 import Bicycle2Svg from "../../svg/bicycle2.svg?react";
 import Bicycle3Svg from "../../svg/bicycle3.svg?react";
 
 export default function Challenge1() {
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 600px)",
+  });
   return (
     <div>
       <Header />
-      <MainVisual />
-      <About />
-      <Bicycle />
+      <MainVisual isDesktopOrLaptop={isDesktopOrLaptop} />
+      <About isDesktopOrLaptop={isDesktopOrLaptop} />
+      <Bicycle isDesktopOrLaptop={isDesktopOrLaptop} />
     </div>
   );
 }
 
 function Header() {
   return (
-    <div className="py-4 px-60">
+    <div className="py-4 px-[10px] flex justify-between align-center">
+      <div>Home</div>
       <div className="flex gap-4 justify-end">
         <AnchorLink href="#link1" offset="50">
           <p className="font-sans text-black">About</p>
@@ -31,52 +38,99 @@ function Header() {
   );
 }
 
-function MainVisual() {
-  // TODO: 親要素の外まで広げたい？
+function MainVisual({ isDesktopOrLaptop }: { isDesktopOrLaptop: boolean }) {
   return (
-    <div className="pt-2 max-h-[600px]">
-      <MainVisualSvg className="max-h-[600px]" />
-    </div>
+    <section className="pt-2">
+      {isDesktopOrLaptop ? (
+        <MainVisualSvg className="w-full" />
+      ) : (
+        <MainVisualSvg2 className="w-full" />
+      )}
+    </section>
   );
 }
 
-function About() {
+function About({ isDesktopOrLaptop }: { isDesktopOrLaptop: boolean }) {
+  if (isDesktopOrLaptop) {
+    return (
+      <section id="link1" className="pt-30 px-[4%]">
+        <p className="text-5xl underline">About</p>
+        <div className="flex pt-10 gap-6">
+          <AboutSvg />
+          <div className="flex flex-col gap-4">
+            <p className="text-2xl text-left">AAAA BBBB</p>
+            <p className="text-left">
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
   return (
-    <div id="link1" className="pt-30 px-60">
-      <p className="text-5xl underline">About</p>
-      <div className="flex pt-10 gap-6">
+    <section id="link1" className="pt-30 px-[4%]">
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <p className="text-5xl underline">About</p>
         <AboutSvg />
-        <div className="flex flex-col gap-4">
-          <p className="text-2xl text-left">AAAA BBBB</p>
-          <p className="text-left">
-            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-          </p>
+        <div className="text-2xl text-left">AAAA BBBB</div>
+        <p className="text-left">
+          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Bicycle({ isDesktopOrLaptop }: { isDesktopOrLaptop: boolean }) {
+  if (isDesktopOrLaptop) {
+    return (
+      <div id="link2" className="pt-30 px-[4%]">
+        <p className="text-5xl underline">Bicycle</p>
+        <div className="flex pt-10 gap-6">
+          <div className="flex flex-col gap-4">
+            <Bicycle1Svg />
+            <p className="text-2xl">タイトル1</p>
+            <p className="">
+              テキストテキストテキストテキストテキストテキストテキスト
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <Bicycle2Svg />
+            <p className="text-2xl">タイトル2</p>
+            <p className="">
+              テキストテキストテキストテキストテキストテキストテキスト
+            </p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <Bicycle3Svg />
+            <p className="text-2xl">タイトル3</p>
+            <p className="">
+              テキストテキストテキストテキストテキストテキストテキスト
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-function Bicycle() {
+    );
+  }
   return (
-    <div id="link2" className="pt-30 px-60">
-      <p className="text-5xl underline">Bicycle</p>
-      <div className="flex pt-10 gap-6">
-        <div className="flex flex-col gap-4">
+    <div id="link2" className="pt-30 px-[4%]">
+      <div className="flex flex-col pt-10 gap-6 items-center">
+        <p className="text-5xl underline">Bicycle</p>
+        <div className="flex flex-col gap-4 items-center">
           <Bicycle1Svg />
           <p className="text-2xl">タイトル1</p>
           <p className="">
             テキストテキストテキストテキストテキストテキストテキスト
           </p>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 items-center">
           <Bicycle2Svg />
           <p className="text-2xl">タイトル2</p>
           <p className="">
             テキストテキストテキストテキストテキストテキストテキスト
           </p>
         </div>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 items-center">
           <Bicycle3Svg />
           <p className="text-2xl">タイトル3</p>
           <p className="">
